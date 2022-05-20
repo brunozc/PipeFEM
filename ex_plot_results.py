2005cc052fe8b7c184549234c5be7658170f7510
Bruno Zuada Coelho <brunozuada@gmail.com>
2022-05-20 10:25:50 +0200

    added amplitudes to csv edited the movie to show displacement

--- ex_plot_results.py
+++ ex_plot_results.py
@@ -1,6 +1,6 @@
 import pickle
 import matplotlib.pylab as plt
-from pipe_fem.post_process import make_movie
+from pipe_fem.post_process import make_movie, write_results_csv
 
 # load results
 with open("./results/data.pickle", "rb") as fi:
@@ -10,7 +10,7 @@ with open("./results/data.pickle", "rb") as fi:
 fig, ax = plt.subplots()
 ax.set_position([0.2, 0.12, 0.7, 0.8])
 ax.plot(data["node 1"]["Time"], data["node 1"]["Displacement"][:, 1], label="node 1")
-ax.plot(data["node 20"]["Time"], data["node 20"]["Displacement"][:, 1], label="node 20")
+ax.plot(data["node 3129"]["Time"], data["node 3129"]["Displacement"][:, 1], label="node 3129")
 ax.plot(data["node 4500"]["Time"], data["node 4500"]["Displacement"][:, 1], label="node 4500")
 ax.set_xlabel("Time [s]")
 ax.set_ylabel("Vertical displacement [m]")
@@ -18,6 +18,10 @@ ax.grid()
 plt.legend()
 plt.show()
 
+# write amplitudes
+write_results_csv(data, 3129, 3150, "results", "amplitudes", number_of_periods=2, frequency=20)
+
 # make movie
 make_movie(data, "results", "displacement_field.gif", scale_fct=1e4, step=2)
-make_movie(data, "results", "displacement_detail_field.gif", scale_fct=1e2, step=2, node_start=3109)
+make_movie(data, "results", "displacement_detail_field.gif", step=2,
+           node_start=3129, node_end=3150, ylabel="Vertical displacement [m]", normalise=True)
--- pipe_fem/post_process.py
+++ pipe_fem/post_process.py
@@ -75,7 +75,8 @@ def plot_time_history_node(results, node, output_folder, name, key="Displacement
     return
 
 
-def make_movie(results, output_folder, name, scale_fct=1000, tmp_folder="tmp", step=10, node_start=None):
+def make_movie(results, output_folder, name, scale_fct=1000, tmp_folder="tmp", step=10,
+               node_start=None, node_end=None, ylabel="Y distance [m]", xlabel="X distance [m]", normalise=False):
 
     if not os.path.isdir(os.path.join(output_folder, tmp_folder)):
         os.makedirs(os.path.join(output_folder, tmp_folder))
@@ -88,14 +89,28 @@ def make_movie(results, output_folder, name, scale_fct=1000, tmp_folder="tmp", s
         idx = nods.index(str(int(node_start)))
         list_nodes = [list_nodes[i] for i in range(idx, len(list_nodes))]
 
+    if node_end is not None:
+        nods = [i.split(" ")[-1] for i in list(list_nodes)]
+        idx = nods.index(str(int(node_end))) + 1
+        list_nodes = [list_nodes[i] for i in range(idx)]
+
     # initial geometry
     x, y = [], []
     for node in list_nodes:
         x.append(results[node]["Coordinates"][0])
         y.append(results[node]["Coordinates"][1])
 
-    for i in range(0, len(time), step):
+    # if normalise: scale_fct = 1 and the y axis displays displacement
+    if normalise:
+        scale_fct = 1
+        x_lim, y_lim = [], []
+        for i in range(0, len(time), step):
+            for node in list_nodes:
+                x_lim.append(results[node]["Coordinates"][0] + results[node]["Displacement"][i, 0] * scale_fct)
+                y_lim.append(results[node]["Coordinates"][1] + results[node]["Displacement"][i, 1] * scale_fct)
 
+    # plot time steps
+    for i in range(0, len(time), step):
         x_t, y_t = [], []
         for node in list_nodes:
             x_t.append(results[node]["Coordinates"][0] + results[node]["Displacement"][i, 0] * scale_fct)
@@ -103,11 +118,15 @@ def make_movie(results, output_folder, name, scale_fct=1000, tmp_folder="tmp", s
 
         fig, ax = plt.subplots()
         ax.plot(x, y, color='k')
-        xlimits = ax.get_xlim()
-        ylimits = ax.get_ylim()
+        if normalise:
+            xlimits = (np.min(x_lim), np.max(x_lim))
+            ylimits = (np.min(y_lim), np.max(y_lim))
+        else:
+            xlimits = ax.get_xlim()
+            ylimits = ax.get_ylim()
         ax.plot(x_t, y_t, color='b')
-        ax.set_xlabel("X distance [m]")
-        ax.set_ylabel("Y distance [m]")
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
         ax.set_title(f"Time: {time[i]: .4f} s")
         ax.set_xlim(xlimits)
         ax.set_ylim(ylimits)
@@ -124,3 +143,35 @@ def make_movie(results, output_folder, name, scale_fct=1000, tmp_folder="tmp", s
     writer.close()
 
     shutil.rmtree(os.path.join(output_folder, tmp_folder))
+
+
+def write_results_csv(results, node_start, node_end, output_folder, name, number_of_periods=2, frequency=20):
+
+    if not os.path.isdir(output_folder):
+        os.makedirs(output_folder)
+
+    # time window
+    time = results["node 1"]["Time"]
+    t_ini = time[-1] - number_of_periods / frequency
+    idx_time = np.where(time > t_ini)[0]
+
+    # collect nodes
+    list_nodes = list(results.keys())
+
+    nods = [i.split(" ")[-1] for i in list(list_nodes)]
+    idx_ini = nods.index(str(int(node_start)))
+    idx_end = nods.index(str(int(node_end))) + 1
+    list_nodes = [list_nodes[i] for i in range(idx_ini, idx_end)]
+
+    # collect max/min in time window for each node and write
+    with open(os.path.join(output_folder, f"{name}.csv"), "w") as fo:
+        fo.write("Node;X coord;Y coord;Z coord;Max Displacement [m];Min Displacement [m]\n")
+        # write results for the nodes
+        for node in list_nodes:
+            fo.write(f"{node};"
+                     f"{results[node]['Coordinates'][0]};"
+                     f"{results[node]['Coordinates'][1]};"
+                     f"{results[node]['Coordinates'][2]};"
+                     f"{np.max(results[node]['Displacement'][idx_time, 1])};"
+                     f"{np.min(results[node]['Displacement'][idx_time, 1])}\n"
+                     )
